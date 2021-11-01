a243b058e8fe77f96d73acc939e543bb6fa87f4f
devriazul <engr.riazul@gmail.com>
2021-11-01 22:33:25 +0600

    Final Update

---
+++ about.php
@@ -0,0 +1,92 @@
+<?php require_once 'header.php';?>
+<!-- start main content -->
+<main class="main-container">
+	<!-- service section -->
+	<div class="container">
+		<!-- What We Do -->
+		<div class="content-md container-sm border-bottom-1">
+			<div class="row">
+				<div class="col-sm-7 sm-margin-b-50">
+					<!-- Heading v1 -->
+					<div class="heading-v1 margin-b-30">
+						<h2 class="heading-v1-title">What we do - It Looks Good on You ...</h2>
+						<p class="heading-v1-subtitle">We create experiences that transform brands, grow businesses and make people’s lives better.</p>
+					</div>
+					<!-- End Heading v1 -->
+
+					<div class="margin-b-30">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+					</div>
+				</div>
+				<div class="col-sm-5">
+					<img class="img-responsive" src="admin/uploads/products/a.jpg" alt="">
+				</div>
+			</div>
+			<!--// end row -->
+		</div>
+		<!-- End What We Do -->
+
+		<!-- Team v2 -->
+		
+		<!-- End Team v2 -->
+
+
+	</div>
+	<!-- end service section -->
+
+
+	<!-- Counters v2 -->
+	<div class="bg-color-sky-light">
+
+		<div class="content-md container">
+			<div class="row">
+				<div class="col-md-3 col-sm-6 md-margin-b-30">
+					<!-- Counters v2 -->
+					<div class="counters-v2">
+						<span class="services-icon-style ti-support"></span>
+						<figure class="counter color-base counters-v2-no">808</figure>
+						<h4 class="counters-v2-title">Design Completed</h4>
+						<span class="counters-v2-subtitle">Great Performance</span>
+					</div>
+					<!-- End Counters v2 -->
+				</div>
+				<div class="col-md-3 col-sm-6 md-margin-b-30">
+					<!-- Counters v2 -->
+					<div class="counters-v2">
+						<span class="services-icon-style ti-support"></span>
+						<figure class="counter color-base counters-v2-no">782</figure>
+						<h4 class="counters-v2-title">Awards Received</h4>
+						<span class="counters-v2-subtitle">Excellence</span>
+					</div>
+					<!-- End Counters v2 -->
+				</div>
+				<div class="col-md-3 col-sm-6 sm-margin-b-30">
+					<!-- Counters v2 -->
+					<div class="counters-v2 ">
+						<span class="services-icon-style ti-support"></span>
+						<figure class="counter color-base counters-v2-no">272</figure>
+						<h4 class="counters-v2-title">Happy Clients</h4>
+						<span class="counters-v2-subtitle">More enthusiasm</span>
+					</div>
+					<!-- End Counters v2 -->
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<!-- Counters v2 -->
+					<div class="counters-v2">
+						<span class="services-icon-style ti-support"></span>
+						<figure class="counter color-base counters-v2-no">312</figure>
+						<h4 class="counters-v2-title">Feature Options</h4>
+						<span class="counters-v2-subtitle">Customizability</span>
+					</div>
+					<!-- End Counters v2 -->
+				</div>
+			</div>
+			<!--// end row -->
+		</div>
+	</div>
+	<!-- End Counters v2 -->
+</main>
+<!-- end main content -->
+<?php require_once 'footer.php';?>
---
+++ shop.php
@@ -0,0 +1,177 @@
+<?php  require_once 'header.php';?>
+
+<!-- start main content -->
+<main class="main-container">
+<section class="men_area pt-40">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-9 col-md-9 col-sm-9">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="kat-shoe-bg imgframe6">
+							<img src="uploads/s.jpg" width="100%" alt="" />
+						</div>
+					</div>
+				</div>
+				<div id="shop-all" class="row" style="margin-top:30px;">
+                    <div id="shop_page_product"></div>
+                    <!-- 	<div class="col-xs-12 col-sm-6 col-md-4 product-item filter-featured">
+						<div class="product-img">
+							<img src="img/shop/index2_product7.png" alt="product">
+							<div class="product-new">
+								new
+							</div>
+							<div class="product-hover">
+								<div class="product-cart">
+									<a class="btn btn-secondary btn-block" href="#">Add To Cart</a>
+								</div>
+							</div>
+						</div>
+
+						<div class="product-bio">
+							<h4>
+								<a href="#">NorthStar Asphalt</a>
+							</h4>
+							<p class="product-price">$150.00</p>
+						</div>
+                    -->
+
+				</div>
+			</div>
+
+			<aside class="col-md-3 sidebar">
+
+				<div class="widget category-widget">
+
+					<h3>Categories</h3>
+
+					<ul id="category-widget">
+						<li id="front_categories"><a href="#">Girls
+						</li>
+					</ul>
+				</div>
+				<!-- /.category widget -->
+
+				<div class="widget">
+
+					<div class="accordion" id="sidebar-collapse-filter">
+
+						<div class="accordion-group panel">
+
+							<div class="accordion-title">Price Filter
+								<a class="accordion-btn open" data-toggle="collapse" href="#price-filter"></a>
+							</div>
+
+							<div class="accordion-body collapse in" id="price-filter">
+
+								<div class="accordion-body-wrapper">
+
+									<div class="filter-price">
+
+										<div id="price-range"></div>
+
+										<div id="filter-range-details" class="row">
+
+											<div class="col-xs-6">
+
+												<div class="filter-price-label">from
+												</div>
+												<input type="text" id="price-range-low" class="form-control">
+											</div>
+
+											<div class="col-xs-6">
+
+												<div class="filter-price-label">to</div>
+												<input type="text" id="price-range-high" class="form-control">
+											</div>
+										</div>
+
+										<div class="filter-price-action">
+											<a href="#" class="btn btn-custom-6 min-width-xss">Ok</a>
+											<a href="#" class="btn btn-custom-6 min-width-xs">Clear</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- /.accordion-group -->
+					</div>
+					<!-- /.accordion -->
+				</div>
+				<!-- /.widget -->
+
+				<div class="information-entry">
+					<div class="information-blocks">
+						<a class="sale-entry vertical" href="#">
+							<span class="hot-mark yellow">hot</span>
+							<span class="sale-price"><span>-40%</span> Valentine <br> Underwear Sale</span>
+							<span class="sale-description">Lorem ipsum dolor sitamet, conse adipisc sed do eiusmod tempor.</span>
+							<img style="height: 120px" class="sale-image" src="img/text-widget-image-5.jpg" alt="">
+						</a>
+					</div>
+				</div>
+
+
+				<!-- /.widget -->
+
+			</aside>
+			<!-- /.col-md-3 -->
+		</div>
+	</div>
+</section>
+
+	<section class="block gray no-padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="content-box no-margin go-simple">
+						<div class="mini-service-sec">
+							<div class="row">
+								<div class="col-md-3">
+									<div class="mini-service">
+										<i  class="fa fa-paper-plane"></i>
+										<div class="mini-service-info">
+											<h3>Worldwide Delivery</h3>
+											<p>unc tincidunt, on cursusau gmetus, lorem Hore</p>
+										</div>
+									</div><!-- Mini Service -->
+								</div>
+								<div class="col-md-3">
+									<div class="mini-service">
+										<i  class="fa  fa-newspaper-o"></i>
+										<div class="mini-service-info">
+											<h3>Worldwide Delivery</h3>
+											<p>unc tincidunt, on cursusa ugmetus, lorem Hore</p>
+										</div>
+									</div><!-- Mini Service -->
+								</div>
+								<div class="col-md-3">
+									<div class="mini-service">
+										<i  class="fa fa-medkit"></i>
+										<div class="mini-service-info">
+											<h3>Friendly Stuff</h3>
+											<p>unc tincidunt, on cursusau gmetus, lorem Hore</p>
+										</div>
+									</div><!-- Mini Service -->
+								</div>
+								<div class="col-md-3">
+									<div class="mini-service">
+										<i  class="fa  fa-newspaper-o"></i>
+										<div class="mini-service-info">
+											<h3>24/h Support</h3>
+											<p>unc tincidunt, on cursusa ugmetus, lorem Hore</p>
+										</div>
+									</div><!-- Mini Service -->
+								</div>
+							</div>
+						</div><!-- Mini Service Sec -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="clearfix"></div>
+</main>
+<!-- end main content -->
+
+<?php  require_once 'footer.php';?>
